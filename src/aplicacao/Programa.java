f9b9ccad68b93a1cccda60c11105876b0cc006f9
NDArtificer <the.artificer@hotmail.com>
2019-05-21 22:46:37 -0300

    Exercicio Lambda Melhorado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -6,19 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import entidades.Funcionarios;
-import model.FuncionarioService;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Funcionarios> funcionario= new ArrayList<>(); 
+		List<Funcionarios> funcionario = new ArrayList<>();
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		FuncionarioService fs = new FuncionarioService();
-		
+
 		System.out.println("Digite o caminho do arquivo: ");
 		String path = sc.nextLine();
 
@@ -31,22 +30,31 @@ public class Programa {
 				double salario = Double.parseDouble(campos[2]);
 				funcionario.add(new Funcionarios(nome, email, salario));
 				line = br.readLine();
-				}
-			double soma = fs.somaFiltrada(funcionario, p-> p.getNome().charAt(0) == 'M');
+			}
+
+			System.out.print("Informe o salario: ");
 			
-			System.out.print("Informe o salario: ");	
 			double sal = sc.nextDouble();
-			funcionario.removeIf(p-> p.getSalario() <= sal);
-			System.out.println("Email das pessoas que recebem mais do que R$"+ String.format("%.2f", sal)+": ");
-			for(Funcionarios f : funcionario) {
-				
-				System.out.println(f.getEmail());
-			}
+			List<String> emails = funcionario.stream()
+					.filter(x -> x.getSalario() > sal)
+					.map(x -> x.getEmail()).sorted()
+					.collect(Collectors.toList());
 			
-			System.out.println("Soma dos salarios das pessoas que começam com a letra 'M': R$" + String.format("%.2f", soma ));
-			}
-		catch (Exception e) {
+			System.out.println("Email das pessoas que o salrio é maior que R$" + String.format("%.2f", sal) + ": ");
+			emails.forEach(System.out::println);
+
+			double soma = funcionario.stream()
+					.filter(x -> x.getNome().charAt(0) == 'M')
+					.map(x -> x.getSalario())
+					.reduce(0.0, (x, y) -> x + y);
+
+			System.out.println("Soma dos salarios das pessoas que começam com a letra 'M': R$" + String.format("%.2f", soma));
+
+		} catch (Exception e) {
 			System.out.println("Erro:" + e.getMessage());
+		}
+		finally {
+			sc.close();
 		}
 	}
 
